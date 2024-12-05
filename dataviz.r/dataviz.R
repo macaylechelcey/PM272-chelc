73d9c3202474b51a05ff1b44a2b9abc380f34d40
macaylechelcey <macaylechelcey@icloud.com>
2024-12-05 15:34:11 +0000

    correlation plot

--- dataviz.r/dataviz.R
+++ dataviz.r/dataviz.R
@@ -22,3 +22,10 @@ ggplot(health_data, aes(x = BMI)) +
 labs(title = "Mean height in Wales is higher than European Average") +
   theme_minimal()
 
+# create a correlation plot
+
+val_cor <- health_data %>% select(BMI,Age,TotChol) %>% 
+  cor(health_data, use = "complete.obs")
+
+ggcorr(val_cor, label = TRUE, label_alpha = TRUE)
+
