2b6468861077ea98dd3011be1d2bd4cb42288e22
Macayle Chelcey <macaylechelcey@Macayles-MacBook-Air.local>
2024-12-05 15:10:20 +0000

    create BMI histogram

---
+++ dataviz.r/dataviz.R
@@ -0,0 +1,21 @@
+install.packages("GGally")
+install.packages("NHANES")
+
+library(tidyverse)
+library(GGally)
+library(readr)
+library(NHANES)
+
+# load NHANES data
+data("NHANES")
+
+View(NHANES)
+
+# create a sub-table that only has BMI, Age, Gender and Cholesterol
+
+health_data <- NHANES %>% select(BMI,Age,Gender,TotChol)
+
+# create a histogram of BMI using ggplot
+
+ggplot(health_data, aes(x = BMI)) +
+  geom_histogram(binwidth = 1)
